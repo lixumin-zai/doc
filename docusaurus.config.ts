2c72dbc4b2d5a88be821d8c589ea5aaceef85589
lismin <lismin-robot>
2025-01-10 10:52:55 +0800

    Auto commit 2025-01-10 10:52:55

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -82,7 +82,7 @@ const config: Config = {
         },
         {to: '/blog', label: 'Blog', position: 'left'},
         {
-          href: 'https://github.com/facebook/docusaurus',
+          href: 'https://github.com/lixumin-zai',
           label: 'GitHub',
           position: 'right',
         },
