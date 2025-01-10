9ce582137db07713252b3d2f0f7211237a0c366b
lismin <lismin-robot>
2025-01-10 16:52:05 +0800

    Auto commit 2025-01-10 16:52:05

---
+++ script/process_data.py
@@ -0,0 +1,19 @@
+import json
+import os
+
+def merge_word():
+    merge_data = []
+    root_path = "./IELTS/"
+    files_name = os.listdir(root_path)
+    for file_name in files_name:
+        file_path = root_path + file_name
+        with open(file_path, "r") as f:
+            word_data = json.load(f)
+            merge_data.extend(word_data)
+    print(len(merge_data))
+    with open("merge.json", "w") as f:
+        json.dump(merge_data, f, ensure_ascii=False)
+    
+        
+if __name__ == "__main__":
+    merge_word()
