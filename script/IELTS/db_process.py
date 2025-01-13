124794c7ce98a730b8da13c17469a36db7b03342
lismin <lismin-robot>
2025-01-13 14:36:27 +0800

    Auto commit 2025-01-13 14:36:27

--- script/IELTS/db_process.py
+++ script/IELTS/db_process.py
@@ -5,7 +5,7 @@ class Database:
     def __init__(self, db_name):
         self.conn = sqlite3.connect(db_name)
         self.cursor = self.conn.cursor()
-        self.create_table()
+        self.create_sentence_table()
 
     def create_sentence_table(self):
         self.cursor.execute('''
@@ -17,10 +17,28 @@ class Database:
         ''')
         self.conn.commit()
 
+    def create_data(self, words, sentence):
+        """
+        创建用户
+        """
+        self.cursor.execute('SELECT * FROM users WHERE wechat_id = ? OR verification_code = ?', 
+                        (wechat_id, verification_code))
+        if self.cursor.fetchone() is not None:
+            raise UserAlreadyExistsError("该用户已存在")
+
+        self.cursor.execute('INSERT INTO users (wechat_id, verification_code) VALUES (?, ?)', (wechat_id, verification_code))
+        self.conn.commit()
+
+    def get_users(self):
+        """
+        查看所有用户信息
+        """
+        self.cursor.execute('SELECT * FROM users')
+        return self.cursor.fetchall()
+
     
 
 
 if __name__ == "__main__":
     db = Database("./sentence.db")
-    db.create_sentence_table()
     words = ["collision", "grip"]
