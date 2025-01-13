5e06b9f08d7d9107229ed1a8a7d5a619e4632675
lismin <lismin-robot>
2025-01-13 13:26:20 +0800

    Auto commit 2025-01-13 13:26:20

--- script/generate_text.py
+++ script/generate_text.py
@@ -60,3 +60,28 @@ class Chator:
             ]
         )
         return response.choices[0].message.content, response.usage.completion_tokens
+
+import random
+from datetime import datetime, timedelta
+
+
+
+def auto_create():
+    root = "/root/project/doc/lismin/docs/IELTS/"
+
+    # 设置2025年1月1日作为起始日期
+    start_date = datetime(2025, 1, 1)
+    # 设置2025年12月31日作为结束日期
+    end_date = datetime(2025, 1, 27)
+
+    # 使用 for 循环遍历所有日期
+    current_date = start_date
+    while current_date <= end_date:
+        time_text = current_date.strftime("%Y-%m-%d")  # 输出日期，格式为 YYYY-MM-DD
+        current_date += timedelta(days=1)  # 增加一天
+
+        
+
+
+if __name__ == "__main__":
+    auto_create()
